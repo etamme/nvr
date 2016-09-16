5174d5d7ce4fa580da55865b8b2e40279a6dee05
Eric Tamme <eric@uphreak.com>
2016-09-16 11:28:26 -0600

    working with subprocess popen

--- nvr.py
+++ nvr.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/python3
 
 import json, os, subprocess, datetime
 
@@ -8,26 +8,29 @@ with open('config.json') as config_file:
 options = config['options']
 cameras = config['cameras']
 
-basedir = options['basedir']
-interval = options['interval']
-runtime = options['runtime']
+openrtsp = str(options['openrtsp'])
+basedir = str(options['basedir'])
+interval = str(options['interval'])
+runtime = str(options['runtime'])
 cwd = os.getcwd()
-today=datetime.date.today()
 
-print "config:\n"
-print "base directory: "+ str(basedir)
-print "file interval: "+ str(interval) + "\n"
-print "total runtime: "+ str(runtime) + "\n"
-print "cameras:\n"
+today="\""+str(datetime.date.today())+"\""
+print("config:\n")
+print("openRTSP: "+ str(openrtsp))
+print("base directory: "+ str(basedir))
+print("file interval: "+ str(interval))
+print("total runtime: "+ str(runtime) + "\n")
+print("cameras:\n")
 
 for camera in cameras:
-  print str(camera['name']) + ' ' + str(camera['url'])
+  print(str(camera['name']) + ' ' + str(camera['url']))
   if not os.path.exists(basedir+camera['name']):
     os.makedirs(basedir+camera['name'])
 
   os.chdir(basedir+camera['name'])
-  print "-v -c -B 10000000 -b 10000000 -4 -Q -f 30 -F \"" + str(today) + "\" -d " + str(runtime) + " -P " + str(interval) + " " + camera["url"]
-  subprocess.Popen(["openRTSP", "-v -c -B 10000000 -b 10000000 -4 -Q -f 30 -F \"" + str(today) + "\" -d " + str(runtime) + " -P " + str(interval) + " " + camera["url"]])
+  url=camera["url"]
+  args=" -v -c -B 10000000 -b 10000000 -4 -Q -f 30 -F "+today+" -d "+runtime+" -P "+interval+" "
+  subprocess.Popen([openrtsp+args+url],shell=True)
 
 os.chdir(cwd)
 
