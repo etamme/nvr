d41aaf6a9ac9b9dd6c61ef4c5299b0369b652641
Eric Tamme <eric@uphreak.com>
2016-09-16 10:29:31 -0600

    nearly working - just need to sort out the popen

--- nvr.py
+++ nvr.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
-import json, os
-from pprint import pprint
+import json, os, subprocess, datetime
 
 with open('config.json') as config_file:    
     config = json.load(config_file)
@@ -10,11 +9,25 @@ options = config['options']
 cameras = config['cameras']
 
 basedir = options['basedir']
-interval = int(options['interval'])
+interval = options['interval']
+runtime = options['runtime']
+cwd = os.getcwd()
+today=datetime.date.today()
 
 print "config:\n"
 print "base directory: "+ str(basedir)
 print "file interval: "+ str(interval) + "\n"
+print "total runtime: "+ str(runtime) + "\n"
 print "cameras:\n"
+
 for camera in cameras:
   print str(camera['name']) + ' ' + str(camera['url'])
+  if not os.path.exists(basedir+camera['name']):
+    os.makedirs(basedir+camera['name'])
+
+  os.chdir(basedir+camera['name'])
+  print "-v -c -B 10000000 -b 10000000 -4 -Q -f 30 -F \"" + str(today) + "\" -d " + str(runtime) + " -P " + str(interval) + " " + camera["url"]
+  subprocess.Popen(["openRTSP", "-v -c -B 10000000 -b 10000000 -4 -Q -f 30 -F \"" + str(today) + "\" -d " + str(runtime) + " -P " + str(interval) + " " + camera["url"]])
+
+os.chdir(cwd)
+
